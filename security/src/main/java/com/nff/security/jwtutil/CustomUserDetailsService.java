eb1aa59c94a30d34974dcff362192630ada63326
pransish <pranishthapashrestha@gmail.com>
2025-07-20 17:54:20 +1000

    basic spring boot setup + UserRepository

--- security/src/main/java/com/nff/security/jwtutil/CustomUserDetailsService.java
+++ security/src/main/java/com/nff/security/jwtutil/CustomUserDetailsService.java
@@ -1,5 +1,6 @@
 package com.nff.security.jwtutil;
 
+import com.nff.security.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
