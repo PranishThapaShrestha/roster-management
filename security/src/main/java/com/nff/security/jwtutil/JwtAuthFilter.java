177719c94dc9987820044b6b117e2b0e26b697bd
pransish <pranishthapashrestha@gmail.com>
2025-07-27 09:55:09 +1000

    request with header starting bearer

--- security/src/main/java/com/nff/security/jwtutil/JwtAuthFilter.java
+++ security/src/main/java/com/nff/security/jwtutil/JwtAuthFilter.java
@@ -4,6 +4,9 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -19,7 +22,24 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     }
     
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
 
+        String header = request.getHeader("Authorization");
+        if(header!=null && header.startsWith("Bearer ")){
+
+            String token = header.substring(7);
+            if(jwtUtil.validateToken(token)){
+
+                String username=jwtUtil.getUsername(token);
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(auth);
+
+
+            }
+
+        }
+        filterChain.doFilter(request,response);
     }
 }
